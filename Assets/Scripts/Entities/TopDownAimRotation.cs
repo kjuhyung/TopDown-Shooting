867103d072a5f8abf28c298177f6e005ef719e9c
kim ju hyung <kim52116@naver.com>
2023-09-18 16:07:50 +0900

    [Add] TopDownAimRotation
    Player 하위 오브젝트
    1. WeaponPivot 하위
     - WeaponSprite
     - BulletSpawnPoint
    활 회전 및 회전에 따라 캐릭터 뒤집기

---
+++ Assets/Scripts/Entities/TopDownAimRotation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownCharacterController _controller;
+
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+    }
+
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+        // 이벤트 구독
+    }
+
+    public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 아크탄젠트 구하기 , 각도
+        // 0 ~ 3.14 라디안 값 * 디그리값 = 0 도부터 360도 Euler 값으로 만듬
+
+        armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        characterRenderer.flipX = armRenderer.flipY;
+        // 활이 90도가 넘어가면 캐릭터도 뒤집기
+        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        // 구한 Euler 값으로 armPivot 의 z값 변경
+    }
+}
