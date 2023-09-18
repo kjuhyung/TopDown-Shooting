7f28567232f59451f84c98434c57901db54b06b4
kim ju hyung <kim52116@naver.com>
2023-09-18 17:10:04 +0900

    [Add] Scriptable Object
    AttackSO
    RangedAttackData
    
    데이터 저장소 준비

---
+++ Assets/ScriptableObjects/Scripts/RangedAttackData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RangedAttackData", menuName = "TopDownController/Attack/Ranged", order = 0)]
+public class RangedAttackData : AttackSO
+{
+    [Header("# Ranged Attack Data")]
+    public string bulletNameTag;
+    public float duration;
+    public float spread;
+    public int numberOfProjectilesPerShot;
+    public float multipleProjectilesAngel;
+    public Color projectileColor;
+}
