974455a76e22aa9cd43b0e42881db15b737d84e7
kim ju hyung <kim52116@naver.com>
2023-09-18 17:11:45 +0900

    [Edit] RangedAttackData
    order 수정

--- Assets/ScriptableObjects/Scripts/RangedAttackData.cs
+++ Assets/ScriptableObjects/Scripts/RangedAttackData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RangedAttackData", menuName = "TopDownController/Attack/Ranged", order = 0)]
+[CreateAssetMenu(fileName = "RangedAttackData", menuName = "TopDownController/Attack/Ranged", order = 1)]
 public class RangedAttackData : AttackSO
 {
     [Header("# Ranged Attack Data")]
