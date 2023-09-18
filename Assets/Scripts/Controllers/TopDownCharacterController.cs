7c76014531743679d3b0c13359cd6c745d8089a2
kim ju hyung <kim52116@naver.com>
2023-09-18 11:46:26 +0900

    [Add] TopDownCharacterController.
    스크립트 기본 작성

--- Assets/Scripts/Controllers/TopDownCharacterController.cs
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+    // event - 외부에서는 호출하지 못하게 막는다.
 
-    void Start()
+    public void CallMoveEvent(Vector2 direction)
     {
-        
+        OnMoveEvent?.Invoke(direction);
+        // null 이 아닐 때만 실행
     }
 
-    void Update()
+    public void CallLookEvent(Vector2 direction)
     {
-        // 1. 트랜스폼을 변경해서 움직이기
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+        OnLookEvent?.Invoke(direction);
     }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // {
+    //// 기본적인 이동 연습 코드
+    // [SerializeField] private float speed = 5f;
+    // void Update()
+    // {
+    //    // 트랜스폼을 변경해서 움직이기
+    //    float x = Input.GetAxisRaw("Horizontal");
+    //    float y = Input.GetAxisRaw("Vertical");
+
+    //    transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+    // }
+    // }
