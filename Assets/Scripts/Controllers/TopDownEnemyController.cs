48355062daa2cec9005bb00434fe948eb6f060ec
kim ju hyung <kim52116@naver.com>
2023-09-21 11:20:11 +0900

    [Edit] EnemyController
    GameManager 전역변수 할당
    player 위치 받아오기

--- Assets/Scripts/Controllers/TopDownEnemyController.cs
+++ Assets/Scripts/Controllers/TopDownEnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TopDownEnemyController : TopDownCharacterController
 {
+    GameManager gameManager;
     protected Transform ClosestTarget { get; private set; }
 
     protected override void Awake()
@@ -13,7 +14,8 @@ public class TopDownEnemyController : TopDownCharacterController
 
     protected virtual void Start()
     {
-
+        gameManager = GameManager.instance;
+        ClosestTarget = gameManager.Player;
     }
 
     protected virtual void FixedUpdate()
@@ -24,10 +26,10 @@ public class TopDownEnemyController : TopDownCharacterController
     protected float DistacneToTarget()
     {
         return Vector3.Distance(transform.position, ClosestTarget.position);
-    }
+    }  // 타겟과 자신 사이의 거리 
     
     protected Vector2 DirectionToTarget()
     {
         return (ClosestTarget.position - transform.position).normalized;
-    }
+    } // 타겟과 자신 사이의 방향
 }
