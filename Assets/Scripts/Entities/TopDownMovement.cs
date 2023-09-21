a2313704df62436d1f23f31e27ed789c25f7cc33
kim ju hyung <kim52116@naver.com>
2023-09-21 15:55:21 +0900

    [Add] 넉백기능 구현
    [Edit] TopDownMovement.cs
    - 넉백값, 지속시간 변수 선언
    - FixedUpdate() 추가 로직
    - ApplyMovement() 추가 로직
    - ApplyKnockback() 메서드 추가

--- Assets/Scripts/Entities/TopDownMovement.cs
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -10,6 +10,9 @@ public class TopDownMovement : MonoBehaviour
     private Vector2 _movementDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
 
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
     private void Awake()
     {
         _controller = GetComponent<TopDownCharacterController>();
@@ -25,16 +28,32 @@ public class TopDownMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement(_movementDirection);
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+        // 넉백 지속시간 이 0보다 크면 감소시키기 
     }
 
     private void Move(Vector2 direction)
     {
         _movementDirection = direction;
     }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        _knockback = - (other.position - transform.position).normalized * power;
+        // 반대 방향으로 power 만큼 넉백
+    }
+
     private void ApplyMovement(Vector2 direction)
     {
         direction = direction * _stats.CurrentStats.speed;
-
+        if (knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }       
         _rigidbody.velocity = direction;
     }
 }
