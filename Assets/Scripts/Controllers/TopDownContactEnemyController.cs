1aae4ebde2d006fb05b6238fa840f083526e4f4a
kim ju hyung <kim52116@naver.com>
2023-09-21 16:50:31 +0900

    [Add] 근접 적 공격 기능
    [Edit] TopDownContactEnemyController.cs
    -OnTriggerEnter2D
    -OnTriggerExit2D
    -ApplyHealthChange

--- Assets/Scripts/Controllers/TopDownContactEnemyController.cs
+++ Assets/Scripts/Controllers/TopDownContactEnemyController.cs
@@ -9,14 +9,30 @@ public class TopDownContactEnemyController : TopDownEnemyController
 
     [SerializeField] private SpriteRenderer characterRenderer;
 
+    private HealthSystem healthSystem;
+    private HealthSystem _collidingTagetHealthSystem;
+    private TopDownMovement _collidingMovement;
+
     protected override void Start()
     {
         base.Start();
+        healthSystem = GetComponent<HealthSystem>();
+        healthSystem.OnDamage += OnDamage;
+    }
+
+    private void OnDamage()
+    {
+        followRange = 100f;
     }
 
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
+
+        if(_isCollidingWithTarget)
+        {
+            ApplyHealthChange();
+        }
 
         Vector2 direction = Vector2.zero;
         if(DistanceToTarget() < followRange)
@@ -32,5 +48,42 @@ public class TopDownContactEnemyController : TopDownEnemyController
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject receiver = collision.gameObject;
+
+        if(!receiver.CompareTag(targetTag))
+        {
+            return;
+        }
+        _collidingTagetHealthSystem = receiver.GetComponent<HealthSystem>();
+        if (_collidingTagetHealthSystem != null )
+        {
+            _isCollidingWithTarget = true;
+        }
+
+        _collidingMovement = receiver.GetComponent<TopDownMovement>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag(targetTag))
+        {
+            return;
+        }
+
+        _isCollidingWithTarget = false;
+    }
+
+    private void ApplyHealthChange()
+    {
+        AttackSO attackSO = Stats.CurrentStats.attackSO;
+        bool hasBeenChanged = _collidingTagetHealthSystem.ChangeHealth(-attackSO.power);
+        if(attackSO.isOnKnockback && _collidingMovement != null )
+        {
+            _collidingMovement.ApplyKnockback(transform, attackSO.knockbackPower,attackSO.knockbackTime);
+        }
     }
 }
