27eea1bab97a3b6c0620bd59c34d53633c57d62a
kim ju hyung <kim52116@naver.com>
2023-09-22 16:26:44 +0900

    [Add] 아이템 생성 기능
    [Edit] GameManager
    - CreateReward() 메서드
    - 5번째 웨이브마다 아이템 생성

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform spawnPositionRoot;
     private List<Transform> spawnPositions = new List<Transform>();
 
+    public List<GameObject> rewards = new List<GameObject> ();
+
     private void Awake()
     {
         instance = this;
@@ -73,7 +75,7 @@ public class GameManager : MonoBehaviour
 
                 if (currentWaveIndex % 5 == 0)
                 {
-                    // CreateReward();
+                    CreateReward();
                 }
 
                 if (currentWaveIndex % 3 == 0)
@@ -131,6 +133,14 @@ public class GameManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    void CreateReward()
+    {
+        int idx = Random.Range(0,rewards.Count);
+        int posIdx = Random.Range(0, spawnPositions.Count);
+
+        GameObject obj = rewards[idx];
+        Instantiate(obj, spawnPositions[posIdx].position, Quaternion.identity);
     }
 
 }
