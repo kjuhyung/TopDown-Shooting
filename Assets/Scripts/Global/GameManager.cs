9d2275191d27c9ccfd6356a0c7bf19125ab1c0d9
kim ju hyung <kim52116@naver.com>
2023-09-22 11:43:21 +0900

    [Edit] UI
    GameManager 에서 관리

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,4 +1,7 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,10 +9,48 @@ public class GameManager : MonoBehaviour
 
     public Transform Player { get; private set; }
     [SerializeField] private string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
 
     private void Awake()
     {
         instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
+
     }
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateWaveUI()
+    {
+        // waveText.text =
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
